0ab8723649afde834c673cfc46d02fb18718f793
Rohan-Kuruvila <151589950+Rohan-Kuruvila@users.noreply.github.com>
2026-03-03 19:25:06 -0600

    Finished logic

--- src/main/java/frc/robot/commands/ShooterHood/PID/FindAngle.java
+++ src/main/java/frc/robot/commands/ShooterHood/PID/FindAngle.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.ShooterHood.PID;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -28,7 +28,9 @@ public class FindAngle extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    shooterHood.updatePID();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -36,12 +38,17 @@ public class FindAngle extends Command {
     Translation2d odoVec = drivetrain.getState().Pose.getTranslation();
     Translation2d targetVector = hubVector.minus(odoVec);
     double distance = targetVector.getNorm();
-    shooterHub.findAngle(distance);
+    double angle = shooterHub.findAngle(distance);
+    if(45 <= angle && 90 >= angle) {
+      shooterHood.goToAngle(distance);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooterHood.stopHood();
+  }
 
   // Returns true when the command should end.
   @Override
