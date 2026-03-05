9148d2c5cb854bdc12dac3048fca3fe0b6f5456d
AnshG55 <anshg1014@gmail.com>
2026-03-05 12:06:39 -0600

    New Data

--- src/main/java/frc/robot/brains/ShooterMath.java
+++ src/main/java/frc/robot/brains/ShooterMath.java
@@ -7,6 +7,7 @@ package frc.robot.brains;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.util.Units;
@@ -16,10 +17,11 @@ public class ShooterMath {
     private final CommandSwerveDrivetrain commandSwerveDrivetrain;
     private final double radius = Units.inchesToMeters(2);
 
+
     //New Measurment Arrays
-    private static final double[] distances = {1,3.5};//,0,0,0};  //meters
-    private static final double[] angles = {0,0.5};//,0,0,0};     //rots from position zero
-    private static final double[] velocities = {16,16};//,0,0,0}; //meters per seconds
+    private static final double[] distances = {1,2,3.5,3.6576};//,0,0,0};  //meters
+    private static final double[] angles = {0,0.009033,0.5,0.050781};//,0,0,0};     //rots from position zero
+    private static final double[] velocities = {16,16,16,16};//,0,0,0}; //meters per seconds
     private static final double[] times = {0,0};//,0,0,0};        //seconds
 
     private final double[] linearizeVel = {velocityLinearizer(velocities[0]),velocityLinearizer(velocities[1])};
@@ -33,15 +35,15 @@ public class ShooterMath {
         //Interpolation Double tree for Velocities
         tableVel.put(distances[0], velocities[0]);
         tableVel.put(distances[1], velocities[1]);
-        //tableVel.put(distances[2], velocities[2]);
-        //tableVel.put(distances[3], velocities[3]);
+        tableVel.put(distances[2], velocities[2]);
+        tableVel.put(distances[3], velocities[3]);
         //tableVel.put(distances[4], velocities[4]);
 
         //Interpolation Double tree for Angles
         tableAngle.put(distances[0], angles[0]);
         tableAngle.put(distances[1], angles[1]);
-        //tableAngle.put(distances[2], angles[2]);
-        //tableAngle.put(distances[3], angles[3]);
+        tableAngle.put(distances[2], angles[2]);
+        tableAngle.put(distances[3], angles[3]);
         //tableAngle.put(distances[4], angles[4]);
 
         //Linearized Velocity Table
@@ -57,16 +59,20 @@ public class ShooterMath {
     
     public ShooterMath(CommandSwerveDrivetrain commandSwerveDrivetrain) {
         this.commandSwerveDrivetrain = commandSwerveDrivetrain;
+        
     }
 
     
     public double getDistanceFromHub() {
-        Translation2d originToBlueHub = new Translation2d(Units.inchesToMeters(181.56),Units.inchesToMeters(158.32));
-        double distanceBlue = commandSwerveDrivetrain.getStatePose().getTranslation().getDistance(originToBlueHub);
-        Translation2d originToRedHub = new Translation2d(Units.inchesToMeters(181.56+287),Units.inchesToMeters(158.32));
-        double distanceRed = commandSwerveDrivetrain.getStatePose().getTranslation().getDistance(originToRedHub);
-
-        return distanceBlue;
+        if(DriverStation.getAlliance().equals(Alliance.Blue)) {
+            Translation2d originToBlueHub = new Translation2d(Units.inchesToMeters(181.56),Units.inchesToMeters(158.32));
+            double distanceBlue = commandSwerveDrivetrain.getStatePose().getTranslation().getDistance(originToBlueHub);
+            return distanceBlue;
+        }else{
+            Translation2d originToRedHub = new Translation2d(Units.inchesToMeters(181.56+287),Units.inchesToMeters(158.32));
+            double distanceRed = commandSwerveDrivetrain.getStatePose().getTranslation().getDistance(originToRedHub);
+            return distanceRed;
+        }
     }
 
     //Umar's Interpolation Request for Velocity
@@ -81,6 +87,8 @@ public class ShooterMath {
     public double AngleInterpolNew() {
         return tableAngle.get(getDistanceFromHub());
     }
+
+    
 
     public void initSendable(SendableBuilder builder) {
     builder.setSmartDashboardType("Shooter Math");
