f7feeb5ea9b2cda35ec62cf5f4c388f076bf7014
AnshG55 <anshg1014@gmail.com>
2026-03-18 18:56:43 -0500

    Create Intakesanrithy.java

---
+++ src/main/java/frc/robot/subsystems/Intakesanrithy.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Intakesanrithy extends SubsystemBase{
+    
+}
