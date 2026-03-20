172da012c11b4e57d28a09476577c272218c88e4
Umar K <umarkhanayatampa@gmail.com>
2026-03-19 19:50:23 -0500

    print statement removal

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -282,7 +282,6 @@ public class Shooter extends SubsystemBase {
 
   @Override
   public void periodic() {
-    System.out.println(getDistanceToHub());
     // This method will be called once per scheduler run
     //updatePID();
   }
