75416d0475010572b844ac11e19a877aa9c09ba6
Rohan-Kuruvila <151589950+Rohan-Kuruvila@users.noreply.github.com>
2026-03-25 15:03:08 -0500

    Moved PowerBank logic into InterpolTargetSpeed

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,21 +13,17 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicVelocityDutyCycle;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -260,7 +256,17 @@ public class Shooter extends SubsystemBase {
     setTargetVelocity(velmps);
     double radspersec = velmps/(radius);
     double rotspersec = Units.radiansToRotations(radspersec);
-    return rotspersec;
+
+    double accelCmd = (rotspersec - slewRateLimiter.lastValue()) / 0.02;
+    accelCmd = MathUtil.clamp(accelCmd, -accelerationRotations, accelerationRotations);
+
+    double powerReq = slewRateLimiter.getPowerFromAcceleration(accelCmd, slewRateLimiter.lastValue());
+    shooterAccount.setMaxRequest(powerReq);
+    double rotAccelMag = slewRateLimiter.getAccelerationFromPower(shooterAccount.getAllowance(), slewRateLimiter.lastValue());
+    double rotAccel = Math.copySign(rotAccelMag, accelCmd);
+
+    double newRotVel = slewRateLimiter.calculate(rotAccel);
+    return newRotVel;
   }
 
   public double getStatorCurrent() {return rightShooter.getStatorCurrent().getValueAsDouble();}
