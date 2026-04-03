6919e8796912f23dac3d5d6235d7d17422a1c64f
Umar K <umarkhanayatampa@gmail.com>
2026-04-02 20:55:00 -0500

    post strat meeting

--- src/main/java/frc/robot/commands/Intake/PID/PivotOut.java
+++ src/main/java/frc/robot/commands/Intake/PID/PivotOut.java
@@ -30,7 +30,7 @@ public class PivotOut extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.stopPivot();
+    //intake.stopPivot();
   }
 
   // Returns true when the command should end.
